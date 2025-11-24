15a3740007af9fcd70d4ba6530a057294c8f3b6c
Rick Bhattacharya <rickbhattacharya8@gmail.com>
2025-11-24 20:55:00 +0530

    Setup user route for registration

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
-import createHttpError, { HttpError } from 'http-errors';
-import { config } from './config/config';
 import globalErrorHandler from './middleware/globalErrorHandler';
+import userRouter from './user/userRouter';
 
 const app=express();
 
 // Routes
 app.get('/',(req,res)=>{
-
     res.send("Wellcome to our Book store management system");
 })
+app.use("/api/users",userRouter);
 
 // global error handler
 app.use(globalErrorHandler);
